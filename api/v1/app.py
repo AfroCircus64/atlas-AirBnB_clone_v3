00da452de1ee497c095ae4b6709168b71fac3e26
JCox924 <joshua.cox@atlasschool.com>
2024-07-16 19:02:40 -0500

    revert

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 """Flask web application"""
-from flask import Flask
-from models import storage
+from flask import Flask, jsonify
 from api.v1.views import app_views
 import os
+from models import storage
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -15,11 +21,32 @@ def teardown_db(exception):
     storage.close()
 
 
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(counts)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 error handler"""
+    return {"error": "Not found"}, 404
+
 def run_flask():
     """Method to run flask"""
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
             port=os.getenv('HBNB_API_PORT', '5000'),
             threaded=True)
+
+
 
 
 if __name__ == "__main__":

9281707d5ac400d38e1ee39a5e730a1344e3e628
JCox924 <joshua.cox@atlasschool.com>
2024-07-16 19:13:05 -0500

    add error handler for not found code (404)

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,7 @@ from flask import Flask, jsonify
 from api.v1.views import app_views
 import os
 from models import storage
-from models.state import State
-from models.city import City
-from models.user import User
-from models.place import Place
-from models.review import Review
-from models.amenity import Amenity
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
@@ -21,25 +16,6 @@ def teardown_db(exception):
     storage.close()
 
 
-@app_views.route('/stats', methods=['GET'], strict_slashes=False)
-def get_stats():
-    """Retrieves the number of each objects by type"""
-    counts = {
-        "amenities": storage.count(Amenity),
-        "cities": storage.count(City),
-        "places": storage.count(Place),
-        "reviews": storage.count(Review),
-        "states": storage.count(State),
-        "users": storage.count(User)
-    }
-    return jsonify(counts)
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """404 error handler"""
-    return {"error": "Not found"}, 404
-
 def run_flask():
     """Method to run flask"""
     app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'),
@@ -47,6 +23,10 @@ def run_flask():
             threaded=True)
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Handler for 404 errors that returns a JSON-formatted response"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,34 @@
 """simple API endpoint"""
 from flask import jsonify
 from api.v1.views import app_views
-
+from models import storage
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Returns the API's status"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def get_stats():
+    """Retrieves the number of each objects by type"""
+    counts = {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User)
+    }
+    return jsonify(counts)
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """404 error handler"""
+    return {"error": "Not found"}, 404

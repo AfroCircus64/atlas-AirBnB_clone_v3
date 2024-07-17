c45f35b968649cbc48df61d569c32d99002f0493
Trey Turner <trey.turner1857@gmail.com>
2024-07-16 23:38:06 -0500

    Forgot the line too long change

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -8,8 +8,7 @@ from models import storage
 from models.city import City
 from models.state import State
 
-@app_views.route(
-        '/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['GET'], strict_slashes=False)
 def get_cities(state_id):
     """ Retrieves the list of all City objects of a State """
     state = storage.get(State, state_id)
@@ -26,8 +25,7 @@ def get_city(city_id):
         abort(404)
     return jsonify(city.to_dict())
 
-@app_views.route(
-        '/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('/cities/<city_id>', methods=['DELETE'], strict_slashes=False)
 def delete_city(city_id):
     """ Deletes a City object """
     city = storage.get(City, city_id)
@@ -37,8 +35,7 @@ def delete_city(city_id):
     storage.save()
     return jsonify({}), 200
 
-@app_views.route(
-        '/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
+@app_views.route('/states/<state_id>/cities', methods=['POST'], strict_slashes=False)
 def post_city(state_id):
     """ Creates a City """
     city_data = request.get_json()
